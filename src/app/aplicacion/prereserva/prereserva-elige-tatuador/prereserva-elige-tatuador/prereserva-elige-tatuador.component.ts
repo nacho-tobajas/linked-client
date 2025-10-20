0a1bcb7639115eb7258d1bc82c8e774da1003fd0
Dmcrow00 <rceballos.work@gmail.com>
2025-10-20 19:31:09 -0300

    actualiza develop

---
+++ src/app/aplicacion/prereserva/prereserva-elige-tatuador/prereserva-elige-tatuador/prereserva-elige-tatuador.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prereserva-elige-tatuador',
+  imports: [],
+  templateUrl: './prereserva-elige-tatuador.component.html',
+  styleUrl: './prereserva-elige-tatuador.component.scss'
+})
+export class PrereservaEligeTatuadorComponent {
+
+}
