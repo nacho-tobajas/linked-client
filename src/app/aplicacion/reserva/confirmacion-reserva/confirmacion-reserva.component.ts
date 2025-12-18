d608fee8ee82fcfe8091acd7011632e3e65d6614
mauroagustin99 <mauromarcosano@gmail.com>
2025-12-18 16:01:23 -0300

    Mejora en confirmacion-reserva estilos

--- src/app/aplicacion/reserva/confirmacion-reserva/confirmacion-reserva.component.ts
+++ src/app/aplicacion/reserva/confirmacion-reserva/confirmacion-reserva.component.ts
@@ -1,37 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-import { Location, NgIf, DatePipe } from '@angular/common';
+import { DatePipe } from '@angular/common';
 import { Router } from '@angular/router';
 import { ReservaStateService } from 'src/app/services/reserva/reserva-state.service';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { ServerUrlPipe } from 'src/app/pipes/server-url.pipe';
 
 @Component({
     selector: 'app-confirmacion-reserva',
     templateUrl: './confirmacion-reserva.component.html',
     styleUrl: './confirmacion-reserva.component.scss',
-    imports: [NgIf, DatePipe]
+    imports: [ 
+        DatePipe, 
+        MatIconModule, 
+        MatButtonModule, 
+        ServerUrlPipe
+    ]
 })
-export class ConfirmacionReservaComponent implements OnInit{
+export class ConfirmacionReservaComponent implements OnInit {
   reserva: any;
 
   constructor(
     private router: Router,
-    private reservaStateService: ReservaStateService,
-    private location: Location
-  ){}
+    private reservaStateService: ReservaStateService
+  ) {}
 
   ngOnInit(): void {  
     this.reserva = this.reservaStateService.getDatos();
 
     if (!this.reserva || !this.reserva.tatuador) {
-      console.warn("No hay datos de reserva en el estado, redirigiendo.");
-      this.router.navigate(['/reserva/listado']);
+      this.router.navigate(['/']);
     }
   }
 
   goBack(): void {
-    this.router.navigate(['/reserva/listado']); 
+    this.router.navigate(['/perfil/mis-turnos']); 
   }
 
   volverInicio(): void {
-    this.router.navigate(['/inicio']); 
+    this.router.navigate(['/']); 
   }
 }
